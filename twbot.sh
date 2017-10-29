654eb76806f153da6707e35d2f0f5be4da44df56
noureddinex <noureddinex@gitlab.com>
2017-10-29 20:55:09 +0200

    new file:   twbot.sh

---
+++ twbot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#### Created by NourEddineX ####
+
+### BEGIN VARS ###
+MIN=30
+MAX=60
+### END VARS ###
+
+while true; do \
+  for i in $(t groupies); do \
+    t follow $i
+    echo followed $i
+    RAND=$(shuf -i $MIN-$MAX -n 1)
+    echo "Sleeping $RAND seconds.."
+    sleep $RAND
+  done
+  sleep $RAND
+  for i in $(t leaders); do \
+    t unfollow $i
+    echo unfollowed $i
+    RAND=$(shuf -i $MIN-$MAX -n 1)
+    echo "Sleeping $RAND seconds.."
+  done
+done
